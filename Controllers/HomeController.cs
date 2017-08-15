e95b3921aac10f4888ad6fca2070fa8d2c5f6472
Troy Center <troycenter1@gmail.com>
2017-08-15 15:35:40 -0700

    working reg and login form

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
+using DbConnection;
+using thewall.Models;
+using System.Linq;
 
 namespace thewall.Controllers
 {
@@ -13,7 +16,71 @@ namespace thewall.Controllers
         [Route("")]
         public IActionResult Index()
         {
+            ViewBag.RegErrors = new List<string>();
             return View();
         }
+
+        [HttpPost]
+        [Route("register")]
+        public IActionResult Register(string firstname, string lastname, string email, string username, string password, string cpassword)
+        {
+            Console.WriteLine("In HOME CONTROLLER Register method ==============================");
+
+            User NewUser = new User
+            {
+                FirstName = firstname,
+                LastName = lastname,
+                UserName = username,
+                Email = email, 
+                Password = password,
+                cPassword = cpassword
+            };
+        
+            TryValidateModel(NewUser); 
+
+            if(ModelState.IsValid){
+                string query = $"INSERT INTO users (firstname, lastname, email, username, password, created_at, updated_at) VALUES ('{firstname}', '{lastname}', '{email}', '{username}', '{password}', NOW(), NOW());";
+                DbConnector.Execute(query);
+
+
+
+            // // DB Query to get user id. and name
+            // Sace to Session. 
+                return View("~/Views/Message/Message.cshtml");    
+            } 
+            else 
+            {
+                ViewBag.RegErrors = ModelState.Values;
+                ViewBag.FirstName = firstname;
+                ViewBag.LastName = lastname;
+                ViewBag.UserName = username;
+                ViewBag.Email = email; 
+                ViewBag.Password = password;
+                ViewBag.cPassword = cpassword;
+                return View("index");
+            }
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public IActionResult Login()
+        {
+            Console.WriteLine("In HOME CONTROLLER Login Method =======================");
+            //check for user account and session data here.... next create quotes controller to handle quotes and check for session...
+
+
+
+            return View();
+        }
+
+        [HttpGet]
+        [Route("logoutorm")]
+        public IActionResult Logout()
+        {
+            //check for user account and session data here.... CLEAR IT and return to login page....
+            return View();
+        }
+
+
     }
 }
